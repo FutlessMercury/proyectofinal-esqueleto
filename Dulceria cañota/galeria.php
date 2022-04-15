87d20994fa3272431d3ec1f93a65b1a93a4a2c4f
FutlessMercury <84038220+FutlessMercury@users.noreply.github.com>
2022-04-15 14:16:26 -0500

    Add files via upload

---
+++ Dulceria cañota/galeria.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Dulceriacanota.com</title>
+</head>
+<body>
+<header class="cabeza">
+    <div>
+    <nav>
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link " aria-current="page" href="index.php">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="somos.php">¿Quiénes somos?</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="catalogo.php">Catálogo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link  " href="sesion.php">Inicio de sesión</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link  " href="galeria.php">Galeria</a>
+  </li>
+</ul>
+    </nav>
+    </div>
+</header>
+<section>
+
+</section>
+
+<footer>
+
+
+</footer>
+</body>
+</html>
---
+++ Dulceria cañota/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Dulceriacanota.com</title>
+</head>
+<body>
+<header class="cabeza">
+    <div>
+    <nav>
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link " aria-current="page" href="index.php">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="somos.php">¿Quiénes somos?</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="catalogo.php">Catálogo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link  " href="sesion.php">Inicio de sesión</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link  " href="galeria.php">Galeria</a>
+  </li>
+</ul>
+    </nav>
+    </div>
+</header>
+<section>
+    <nav>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/portada1" class="d-block w-100" alt="portada2">
+    </div>
+    <div class="carousel-item">
+      <img src="img/portada2" class="d-block w-100" alt="portada3">
+    </div>
+    <div class="carousel-item">
+      <img src="img/portada3" class="d-block w-100" alt="portada1">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </nav>
+</section>
+
+<footer>
+
+
+</footer>
+</body>
+</html>
